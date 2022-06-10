a0d498ed52834521933c7881973e836b32200278
Andres Castro <andacast@espol.edu.ec>
2022-06-09 21:58:02 -0500

    hector map + lidar package

---
+++ car_sensors/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kastros/ws_autonomous_car/car_sensors/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
