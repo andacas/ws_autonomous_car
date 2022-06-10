aa97146a18447e448bb2f4bcb9e7b093ef4af50e
Andres Castro <andacast@espol.edu.ec>
2022-06-09 22:41:17 -0500

    build and devel deleted

--- car_sensors/build/lidar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kastros/ws_autonomous_car/car_sensors/src/lidar
-# Build directory: /home/kastros/ws_autonomous_car/car_sensors/build/lidar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
